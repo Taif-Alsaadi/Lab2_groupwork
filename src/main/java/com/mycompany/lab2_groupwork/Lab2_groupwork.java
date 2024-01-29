062878899b6a3b3514395555373b9a80f2da37d9
Taif Hameed Alsaadi <taif124al@gmail.com>
2024-01-29 21:43:35 +0300

    empty project

--- src/main/java/com/mycompany/lab2_groupwork/Lab2_groupwork.java
+++ src/main/java/com/mycompany/lab2_groupwork/Lab2_groupwork.java
@@ -5,7 +5,7 @@ package com.mycompany.lab2_groupwork;
 public class Lab2_groupwork {
 
     public static void main(String[] args) {
-        System.out.println("Leader Taif");
+        System.out.println("Taif");
         System.out.println("Rama");
 
     }
